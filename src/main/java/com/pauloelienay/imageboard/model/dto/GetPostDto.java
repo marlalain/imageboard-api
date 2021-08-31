a67b55c590d54705f70fa3acf540da6d543c7277
Paulo Elienay <paulo@pauloelienay.com>
2021-08-31 18:18:47 -0300

    feat: add `replies` to `GetPostDto`

--- src/main/java/com/pauloelienay/imageboard/model/dto/GetPostDto.java
+++ src/main/java/com/pauloelienay/imageboard/model/dto/GetPostDto.java
@@ -1,10 +1,13 @@
 package com.pauloelienay.imageboard.model.dto;
 
 import com.pauloelienay.imageboard.model.Post;
+import com.pauloelienay.imageboard.model.Reply;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
 
 @Builder
 @Getter
@@ -15,6 +18,7 @@ public class GetPostDto {
     private String body;
     private String mediaLink;
     private GetUserDto author;
+    private Set<Reply> replies;
 
     public static GetPostDto convertToDto(Post post) {
         return GetPostDto.builder()
@@ -22,6 +26,7 @@ public class GetPostDto {
                 .body(post.getBody())
                 .mediaLink(post.getMediaLink())
                 .author(GetUserDto.convertToDto(post.getAuthor()))
+                .replies(post.getReplies())
                 .build();
     }
 }
