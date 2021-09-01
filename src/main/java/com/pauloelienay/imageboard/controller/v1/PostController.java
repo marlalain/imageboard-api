9a343718d9d617d63499e64162efd75c1b5ce442
Paulo Elienay <paulo@pauloelienay.com>
2021-09-01 18:41:15 -0300

    feat: adds HATEOAS to `PostController`

--- src/main/java/com/pauloelienay/imageboard/controller/v1/PostController.java
+++ src/main/java/com/pauloelienay/imageboard/controller/v1/PostController.java
@@ -8,12 +8,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -24,13 +25,14 @@ public class PostController {
     private final PostService service;
 
     @GetMapping
-    public ResponseEntity<Page<Post>> getPageablePosts(@PageableDefault(size = 10, sort = {"id"}) Pageable pageable) {
-        return new ResponseEntity<>(service.getPageablePosts(pageable), HttpStatus.OK);
+    public ResponseEntity<Page<EntityModel<GetPostDto>>> getPageablePosts
+            (@PageableDefault(size = 10, sort = {"id"}) Pageable pageable) {
+        return new ResponseEntity<>(service.getPageablePosts(pageable).map(this::addLinks), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GetPostDto> getPostById(@PathVariable long id) {
-        return new ResponseEntity<>(service.getPostById(id), HttpStatus.OK);
+    public ResponseEntity<EntityModel<GetPostDto>> getPostById(@PathVariable long id) {
+        return new ResponseEntity<>(addLinksById(id), HttpStatus.OK);
     }
 
     @PostMapping
@@ -44,5 +46,17 @@ public class PostController {
     public ResponseEntity<Void> deletePost(@PathVariable long id) {
         service.deletePostById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    EntityModel<GetPostDto> addLinks(Post post) {
+        return addLinksById(post.getId());
+    }
+
+    EntityModel<GetPostDto> addLinksById(long id) {
+        EntityModel<GetPostDto> model = EntityModel.of(GetPostDto.convertToDto(service.getPostById(id)));
+        model.add(linkTo(methodOn(this.getClass()).getPostById(id)).withSelfRel());
+        model.add(linkTo(methodOn(this.getClass()).deletePost(id)).withRel("delete").withType("DELETE"));
+        model.add(linkTo(methodOn(this.getClass()).getPageablePosts(null)).withRel("posts"));
+        return model;
     }
 }
